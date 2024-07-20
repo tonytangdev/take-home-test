e65f7e00335fac5bf4a016f74129878aff096983
Tony Tang <tony.tang.developer@gmail.com>
2024-07-20 09:41:32 +0200

    test(generic_drug): benefit decreased twice as fast after expiration date is hit

--- src/pharmacy.test.ts
+++ src/pharmacy.test.ts
@@ -1,9 +1,17 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
-    );
+  describe("generic drug", () => {
+    it("should decrease the benefit and expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
+
+    it("should decrease the benefit twice as fast after the expiration date", () => {
+      expect(
+        new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", -1, 1)]);
+    });
   });
 });
