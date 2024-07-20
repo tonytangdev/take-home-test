260ee00f079ee28ba03b3d52cb5765917b98c75a
Tony Tang <tony.tang.developer@gmail.com>
2024-07-20 11:57:38 +0200

    feat: update script to get output.json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Pharmacy } from "./pharmacy";
 import { writeFile } from "fs";
 import { Drug } from "./entities/Drug";
+import { Fervex, HerbalTea, MagicPill } from "./entities";
 
 const drugs = [
   new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40),
+  new HerbalTea(10, 5),
+  new Fervex(5, 40),
+  new MagicPill(15, 40),
 ];
 const trial = new Pharmacy(drugs);
 
